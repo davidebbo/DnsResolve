21b1e24754244d51da70a199d71d4ec05f1b1dab
davidebbo <david.ebbo@microsoft.com>
2013-12-04 15:49:04 -0800

    Initial commit

---
+++ DnsResolve/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnsResolve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPHostEntry hostInfo = Dns.GetHostEntry(args[0]);
+                Console.WriteLine(hostInfo.AddressList[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
